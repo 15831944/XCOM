ef29f24852f1d589dd54419e9298d4ac7f5c5b66
Özgür Özçıtak <oozcitak@gmail.com>
2015-04-09 13:36:18 +0300

    Added XCOM command for protecting drawings.

---
+++ RebarPosCommands/DoubleBufferedFlowLayoutPanel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RebarPosCommands
+{
+    public class DoubleBufferedFlowLayoutPanel : FlowLayoutPanel
+    {
+        private const int WM_SETREDRAW = 0x000B;
+
+        public DoubleBufferedFlowLayoutPanel()
+        {
+            DoubleBuffered = true;
+        }
+
+        public void Suspend()
+        {
+            this.SuspendLayout();
+
+            SuspendControl(this);
+
+            foreach (Control control in this.Controls)
+            {
+                SuspendControl(control);
+            }
+        }
+
+        public void Resume()
+        {
+            foreach (Control control in this.Controls)
+            {
+                ResumeControl(control);
+            }
+
+            ResumeControl(this);
+            this.ResumeLayout();
+        }
+
+        public static void SuspendControl(Control control)
+        {
+            Message msgSuspendUpdate = Message.Create(control.Handle, WM_SETREDRAW, IntPtr.Zero, IntPtr.Zero);
+            NativeWindow window = NativeWindow.FromHandle(control.Handle);
+            window.DefWndProc(ref msgSuspendUpdate);
+        }
+
+        public static void ResumeControl(Control control)
+        {
+            Message msgResumeUpdate = Message.Create(control.Handle, WM_SETREDRAW, new IntPtr(1), IntPtr.Zero);
+            NativeWindow window = NativeWindow.FromHandle(control.Handle);
+            window.DefWndProc(ref msgResumeUpdate);
+
+            control.Invalidate();
+        }
+    }
+}
---
+++ XCOM/Actions/ProtectDrawing.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+
+namespace XCOM
+{
+    public class ProtectDrawing : IXCOMAction
+    {
+        public string Name { get { return "Dosya Koruması"; } }
+        public int Order { get { return 300000; } }
+        public bool Recommended { get { return false; } }
+        public ActionInterface Interface { get { return ActionInterface.Command; } }
+        public bool ShowDialog() { return true; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public string[] Run(string filename, Database db)
+        {
+            List<string> errors = new List<string>();
+
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                try
+                {
+                    // Get all items in this space
+                    BlockTableRecord btrModel = (BlockTableRecord)tr.GetObject(SymbolUtilityServices.GetBlockModelSpaceId(db), OpenMode.ForWrite);
+                    ObjectIdCollection items = new ObjectIdCollection();
+                    foreach (ObjectId id in btrModel)
+                    {
+                        items.Add(id);
+                    }
+
+                    if (items.Count > 0)
+                    {
+                        // Create the unexlodable anon block
+                        BlockTableRecord btrProtected = new BlockTableRecord();
+                        btrProtected.Name = "*U";
+                        btrProtected.Explodable = false;
+
+                        // Add the new block to the block table
+                        BlockTable bt = (BlockTable)tr.GetObject(db.BlockTableId, OpenMode.ForWrite);
+                        ObjectId btrId = bt.Add(btrProtected);
+                        tr.AddNewlyCreatedDBObject(btrProtected, true);
+
+                        // Add all entities in this space to the anon block
+                        btrProtected.AssumeOwnershipOf(items);
+
+                        // Add the block reference to this space
+                        BlockReference br = new BlockReference(Autodesk.AutoCAD.Geometry.Point3d.Origin, btrId);
+                        btrModel.AppendEntity(br);
+                        tr.AddNewlyCreatedDBObject(br, true);
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    errors.Add(ex.ToString());
+                }
+
+                tr.Commit();
+            }
+
+            return errors.ToArray();
+        }
+    }
+}
