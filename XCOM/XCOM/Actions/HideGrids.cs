8a07f4e420a1a98ce361aec0ec57bfc12cd491aa
Özgür Özçıtak <oozcitak@gmail.com>
2016-09-20 15:48:54 +0300

    Added the viewport lock and grid hide actions.

---
+++ XCOM/XCOM/Actions/HideGrids.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+
+namespace XCOM.Commands.XCommand
+{
+    public class HideGrids : IXCOMAction
+    {
+        public string Name { get { return "Grid'leri Gizle"; } }
+        public int Order { get { return 152000; } }
+        public bool Recommended { get { return true; } }
+        public ActionInterface Interface { get { return ActionInterface.Command; } }
+        public bool ShowDialog() { return true; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public void Run(string filename, Database db)
+        {
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                try
+                {
+                    DBDictionary layoutDict = (DBDictionary)tr.GetObject(db.LayoutDictionaryId, OpenMode.ForRead);
+                    foreach (DBDictionaryEntry entry in layoutDict)
+                    {
+                        if (entry.Key.ToUpperInvariant() != "MODEL")
+                        {
+                            Layout layout = (Layout)tr.GetObject(entry.Value, OpenMode.ForRead);
+                            foreach (ObjectId vpId in layout.GetViewports())
+                            {
+                                Viewport vp = (Viewport)tr.GetObject(vpId, OpenMode.ForWrite);
+                                vp.GridOn = false;
+                            }
+                        }
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    OnError(ex);
+                }
+
+                tr.Commit();
+            }
+        }
+
+        public event EventHandler<ActionProgressEventArgs> Progress;
+        public event EventHandler<ActionErrorEventArgs> Error;
+
+        protected void OnProgress(string message)
+        {
+            EventHandler<ActionProgressEventArgs> handler = Progress;
+            if (handler != null)
+            {
+                handler(this, new ActionProgressEventArgs(message));
+            }
+        }
+
+        protected void OnError(System.Exception error)
+        {
+            EventHandler<ActionErrorEventArgs> handler = Error;
+            if (handler != null)
+            {
+                handler(this, new ActionErrorEventArgs(error));
+            }
+        }
+    }
+}
---
+++ XCOM/XCOM/Actions/LockAllViewports.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+
+namespace XCOM.Commands.XCommand
+{
+    public class LockAllViewports : IXCOMAction
+    {
+        public string Name { get { return "Viewport'ları Kilitle"; } }
+        public int Order { get { return 151000; } }
+        public bool Recommended { get { return true; } }
+        public ActionInterface Interface { get { return ActionInterface.Command; } }
+        public bool ShowDialog() { return true; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public void Run(string filename, Database db)
+        {
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                try
+                {
+                    DBDictionary layoutDict = (DBDictionary)tr.GetObject(db.LayoutDictionaryId, OpenMode.ForRead);
+                    foreach (DBDictionaryEntry entry in layoutDict)
+                    {
+                        if (entry.Key.ToUpperInvariant() != "MODEL")
+                        {
+                            Layout layout = (Layout)tr.GetObject(entry.Value, OpenMode.ForRead);
+                            foreach (ObjectId vpId in layout.GetViewports())
+                            {
+                                Viewport vp = (Viewport)tr.GetObject(vpId, OpenMode.ForWrite);
+                                vp.Locked = true;
+                            }
+                        }
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    OnError(ex);
+                }
+
+                tr.Commit();
+            }
+        }
+
+        public event EventHandler<ActionProgressEventArgs> Progress;
+        public event EventHandler<ActionErrorEventArgs> Error;
+
+        protected void OnProgress(string message)
+        {
+            EventHandler<ActionProgressEventArgs> handler = Progress;
+            if (handler != null)
+            {
+                handler(this, new ActionProgressEventArgs(message));
+            }
+        }
+
+        protected void OnError(System.Exception error)
+        {
+            EventHandler<ActionErrorEventArgs> handler = Error;
+            if (handler != null)
+            {
+                handler(this, new ActionErrorEventArgs(error));
+            }
+        }
+    }
+}
