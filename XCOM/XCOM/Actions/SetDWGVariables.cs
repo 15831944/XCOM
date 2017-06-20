1520b413d26a5cdde442c665969588ab74d5cee7
Özgür Özçıtak <oozcitak@gmail.com>
2017-06-20 13:58:32 +0300

    Added the set dwg variables XCOM action.

---
+++ XCOM/XCOM/Actions/SetDWGVariables.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+
+namespace XCOM.Commands.XCommand
+{
+    public class SetDWGVariables : XCOMActionBase
+    {
+        public override string Name { get { return "Set DWG Variables"; } }
+        public override int Order { get { return 152000; } }
+        public override bool Recommended { get { return true; } }
+
+        public override void Run(string filename, Database db)
+        {
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                try
+                {
+                    db.Regenmode = true;
+                }
+                catch (System.Exception ex)
+                {
+                    OnError(ex);
+                }
+
+                tr.Commit();
+            }
+        }
+    }
+}
