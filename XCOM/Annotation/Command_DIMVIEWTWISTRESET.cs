26adae6dbe5c0bfd83efb3ed740297d22d677e0c
Ozgur Ozcitak <ozcitak@yahoo.com>
2018-09-14 12:28:59 +0300

    Added the RESETDIMVIEWTWIST command.

---
+++ XCOM/Annotation/Command_DIMVIEWTWISTRESET.cs
@@ -0,0 +1,45 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace XCOM.Commands.Annotation
+{
+    public class Command_RESETDIMVIEWTWIST
+    {
+        [CommandMethod("RESETDIMVIEWTWIST", CommandFlags.UsePickSet)]
+        public void ResetViewTwist()
+        {
+            var doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            var db = doc.Database;
+
+            List<TypedValue> tvs = new List<TypedValue>();
+            tvs.Add(new TypedValue((int)DxfCode.Start, "DIMENSION"));
+            SelectionFilter filter = new SelectionFilter(tvs.ToArray());
+
+            PromptSelectionResult selRes = doc.Editor.GetSelection(filter);
+            if (selRes.Status == PromptStatus.OK)
+            {
+                using (Transaction tr = db.TransactionManager.StartTransaction())
+                using (ViewTableRecord view = doc.Editor.GetCurrentView())
+                {
+                    try
+                    {
+                        foreach (ObjectId id in selRes.Value.GetObjectIds())
+                        {
+                            Dimension dim = tr.GetObject(id, OpenMode.ForWrite) as Dimension;
+                            dim.HorizontalRotation = view.ViewTwist;
+                        }
+                    }
+                    catch (System.Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.ToString(), "XCOM", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                    tr.Commit();
+                }
+            }
+        }
+    }
+}
