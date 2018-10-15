38e9ffd0e81923e77f46f86cdcbbddecd8380472
Ozgur Ozcitak <ozcitak@yahoo.com>
2018-10-15 18:19:18 +0300

    Added icons to block and layer controls.

--- AcadUtility/WinForms/BlockComboBox.cs
+++ AcadUtility/WinForms/BlockComboBox.cs
@@ -1,5 +1,9 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
+using System.Windows.Forms;
 
 namespace AcadUtility.WinForms
 {
@@ -16,16 +20,75 @@ namespace AcadUtility.WinForms
 
         public BlockComboBox()
         {
+            this.DrawMode = DrawMode.OwnerDrawVariable;
+
             IncludeAnonymous = false;
             IncludeXRef = false;
             IncludeLayout = false;
 
             try
             {
-                var blockNames = AcadSymbolTable.GetBlockTableRecords(this.Database,
+                var blocks = AcadSymbolTable.GetBlockTableRecords(this.Database,
                     p => (IncludeXRef || !p.IsFromExternalReference) && (IncludeXRef || !p.IsFromOverlayReference) && (IncludeLayout || !p.IsLayout) && (IncludeAnonymous || !p.IsAnonymous),
-                    p => p.Name).OrderBy(p => p);
+                    p => new { p.Name, p.HasPreviewIcon, p.PreviewIcon }).OrderBy(p => p.Name);
+                var icons = new List<Bitmap>();
+                var blockNames = new List<string>();
+                foreach (var block in blocks)
+                {
+                    blockNames.Add(block.Name);
+                    icons.Add(block.HasPreviewIcon ? block.PreviewIcon : null);
+                }
                 this.SetItems(blockNames);
+
+                int iconMargin = 2;
+                int textMargin = 2;
+                var iconWidth = 24;
+
+                int dropDownWidth = 0;
+                foreach (var obj in base.Items)
+                {
+                    var width = iconMargin + iconWidth + textMargin + TextRenderer.MeasureText((string)obj, this.Font).Width + textMargin;
+                    dropDownWidth = Math.Max(dropDownWidth, width);
+                }
+                if (base.Items.Count > base.MaxDropDownItems)
+                {
+                    dropDownWidth += SystemInformation.VerticalScrollBarWidth;
+                }
+                dropDownWidth = Math.Min(dropDownWidth, 240);
+                this.DropDownWidth = dropDownWidth;
+
+                this.MeasureItem += (o, e) =>
+                {
+                    var size = TextRenderer.MeasureText(e.Graphics, blockNames[e.Index], this.Font);
+                    e.ItemHeight = size.Height + 2 * textMargin;
+                    e.ItemWidth = iconMargin + iconWidth + textMargin + size.Width + textMargin;
+                };
+
+                this.DrawItem += (o, e) =>
+                {
+                    e.DrawBackground();
+                    Bitmap icon = icons[e.Index];
+                    if (icon != null)
+                    {
+                        var iconSize = icon.Size;
+                        var iconHeight = e.Bounds.Height - 2 * textMargin;
+                        var iconRect = new Rectangle(e.Bounds.Left + iconMargin, e.Bounds.Top + (e.Bounds.Height - iconHeight) / 2, iconWidth, iconHeight);
+
+                        float xScale = iconRect.Width / (float)iconSize.Width;
+                        float yScale = iconRect.Height / (float)iconSize.Height;
+                        float scale = Math.Min(xScale, yScale);
+                        var targetRect = new Rectangle(iconRect.Left + (int)((iconRect.Width - scale * iconSize.Width) / 2),
+                            iconRect.Top + (int)((iconRect.Height - scale * iconSize.Height) / 2),
+                            (int)(scale * iconSize.Width), (int)(scale * iconSize.Height));
+                        e.Graphics.DrawImage(icon, targetRect);
+                        e.Graphics.DrawRectangle(SystemPens.WindowText, iconRect);
+                    }
+
+                    var textRect = new Rectangle(e.Bounds.Left + iconMargin + iconWidth + textMargin, e.Bounds.Top, e.Bounds.Width - (iconMargin + iconWidth + textMargin), e.Bounds.Height);
+                    TextRenderer.DrawText(e.Graphics, blockNames[e.Index], e.Font, textRect, e.ForeColor, e.BackColor, TextFormatFlags.VerticalCenter | TextFormatFlags.Left);
+
+                    e.DrawFocusRectangle();
+                };
             }
             catch
             {
--- AcadUtility/WinForms/LayerComboBox.cs
+++ AcadUtility/WinForms/LayerComboBox.cs
@@ -1,5 +1,9 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
+using System.Windows.Forms;
 
 namespace AcadUtility.WinForms
 {
@@ -10,14 +14,60 @@ namespace AcadUtility.WinForms
 
         public LayerComboBox()
         {
+            this.DrawMode = DrawMode.OwnerDrawVariable;
+
             IncludeXRef = false;
 
             try
             {
-                var layerNames = AcadSymbolTable.GetLayerTableRecords(this.Database,
-                p => (IncludeXRef || !p.IsDependent),
-                p => p.Name).OrderBy(p => p);
+                var layers = AcadSymbolTable.GetLayerTableRecords(this.Database, p => (IncludeXRef || !p.IsDependent), p => new { p.Name, p.Color }).OrderBy(p => p.Name);
+                var colors = new List<Color>();
+                var layerNames = new List<string>();
+                foreach (var layer in layers)
+                {
+                    layerNames.Add(layer.Name);
+                    colors.Add(layer.Color.ColorValue);
+                }
                 this.SetItems(layerNames);
+
+                int iconMargin = 2;
+                int textMargin = 2;
+                var iconWidth = 24;
+
+                int dropDownWidth = 0;
+                foreach (var obj in base.Items)
+                {
+                    var width = iconMargin + iconWidth + textMargin + TextRenderer.MeasureText((string)obj, this.Font).Width + textMargin;
+                    dropDownWidth = Math.Max(dropDownWidth, width);
+                }
+                if (base.Items.Count > base.MaxDropDownItems)
+                {
+                    dropDownWidth += SystemInformation.VerticalScrollBarWidth;
+                }
+                dropDownWidth = Math.Min(dropDownWidth, 240);
+                this.DropDownWidth = dropDownWidth;
+
+                this.MeasureItem += (o, e) =>
+                {
+                    var size = TextRenderer.MeasureText(e.Graphics, layerNames[e.Index], this.Font);
+                    e.ItemHeight = size.Height + 2 * textMargin;
+                    e.ItemWidth = iconMargin + iconWidth + textMargin + size.Width + textMargin;
+                };
+
+                this.DrawItem += (o, e) =>
+                {
+                    e.DrawBackground();
+                    using (var brush = new SolidBrush(colors[e.Index]))
+                    {
+                        var iconHeight = e.Bounds.Height - 2 * textMargin;
+                        var iconRect = new Rectangle(e.Bounds.Left + iconMargin, e.Bounds.Top + (e.Bounds.Height - iconHeight) / 2, iconWidth, iconHeight);
+                        var textRect = new Rectangle(e.Bounds.Left + iconMargin + iconWidth + textMargin, e.Bounds.Top, e.Bounds.Width - (iconMargin + iconWidth + textMargin), e.Bounds.Height);
+                        e.Graphics.FillRectangle(brush, iconRect);
+                        e.Graphics.DrawRectangle(SystemPens.WindowText, iconRect);
+                        TextRenderer.DrawText(e.Graphics, layerNames[e.Index], e.Font, textRect, e.ForeColor, e.BackColor, TextFormatFlags.VerticalCenter | TextFormatFlags.Left);
+                    }
+                    e.DrawFocusRectangle();
+                };
             }
             catch
             {
